451f58ea2b41b710ea854e0f4b922286d3b16678
MLB00N <rwiegel@gmx.de>
2021-12-16 13:53:56 +0100

    refactor: clean up code

--- fashup/src/components/App.js
+++ fashup/src/components/App.js
@@ -1,16 +1,47 @@
 import React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
-import Container from "@mui/material/Container";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+
 import SearchBar from "./SearchBar";
 import ItemSelect from "./ItemSelect";
 
 function App() {
   return (
-    <div>
-      <h1>FashUp</h1>
-      <SearchBar />
-      <ItemSelect />
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative" style={{ backgroundColor: "#51b7ae" }}>
+        <Toolbar>
+          <Typography variant="h5">FashUp</Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        position="relative"
+        component="form"
+        sx={{
+          mt: "5%",
+          ml: "10%",
+          display: "flex",
+          flexDirection: "row",
+          flexGrow: 1,
+          width: "80%",
+          justifyContent: "center",
+        }}
+      >
+        <Grid container spacing={1}>
+          <Grid item xs={12} sm={9}>
+            <SearchBar />
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <ItemSelect />
+          </Grid>
+        </Grid>
+      </Box>
+      ;
+    </>
   );
 }
 
