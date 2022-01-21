938adc8b215355a707678f6d04d90a8414aa6792
Cem <ulrich_345@web.de>
2022-01-21 21:31:21 +0100

    add tabs to header

--- fashup/src/components/Header/Header.jsx
+++ fashup/src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Box, AppBar, Toolbar } from "@mui/material";
+import { Button, Box, AppBar, Toolbar, Tabs, Tab } from "@mui/material";
 import "./Header.css";
 import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
 
@@ -9,22 +9,14 @@ const Header = () => {
       <AppBar className="header">
         <Toolbar>
           <Box className="logo">FASH UP</Box>
-          <div className="categories">
-            <ul>
-              <li>
-                <a href="/">Kids</a>
-              </li>
-              <li>
-                <a href="/">Women</a>
-              </li>
-              <li>
-                <a href="/">Men</a>
-              </li>
-              <li>
-                <a href="/">Cart</a>
-              </li>
-            </ul>
-          </div>
+          <Box className="categories">
+            <Tabs>
+              <Tab label="About Us"/>
+              <Tab label="Our API"/>
+              <Tab label="Documentation" />
+              <Tab label="Help" />
+            </Tabs>
+          </Box>
           <Box className="signUp">
             <Button
               variant="outlined"
