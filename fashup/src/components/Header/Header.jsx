9224ae85eef43e86ad3f45e14bbeff9e9974c574
Cem <ulrich_345@web.de>
2022-01-21 21:13:16 +0100

    add AppBar and Toolbar

--- fashup/src/components/Header/Header.jsx
+++ fashup/src/components/Header/Header.jsx
@@ -1,38 +1,42 @@
-import { Button, Box } from "@mui/material";
 import React from "react";
+import { Button, Box, AppBar, Toolbar } from "@mui/material";
 import "./Header.css";
 import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
 
 const Header = () => {
   return (
-    <Box className="header">
-      <div className="logo">FASH UP</div>
-      <div className="categories">
-        <ul>
-          <li>
-            <a href="/">Kids</a>
-          </li>
-          <li>
-            <a href="/">Women</a>
-          </li>
-          <li>
-            <a href="/">Men</a>
-          </li>
-          <li>
-            <a href="/">Cart</a>
-          </li>
-        </ul>
-      </div>
-      <div className="signUp">
-        <Button
-          variant="outlined"
-          color="primary"
-          startIcon={<PersonAddAltIcon />}
-        >
-          Sign Up
-        </Button>
-      </div>
-    </Box>
+    <React.Fragment>
+      <AppBar className="header">
+        <Toolbar>
+          <Box className="logo">FASH UP</Box>
+          <div className="categories">
+            <ul>
+              <li>
+                <a href="/">Kids</a>
+              </li>
+              <li>
+                <a href="/">Women</a>
+              </li>
+              <li>
+                <a href="/">Men</a>
+              </li>
+              <li>
+                <a href="/">Cart</a>
+              </li>
+            </ul>
+          </div>
+          <Box className="signUp">
+            <Button
+              variant="outlined"
+              color="primary"
+              startIcon={<PersonAddAltIcon />}
+            >
+              Sign Up
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
   );
 };
 
