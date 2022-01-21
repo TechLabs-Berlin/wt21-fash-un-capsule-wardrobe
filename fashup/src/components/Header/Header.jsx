5fcf10eaf881ab3a8ce9dc69dafbbb7a544296a5
Cem <ulrich_345@web.de>
2022-01-21 22:46:14 +0100

    comment out header css and flexbox | add logo tuerkis header directory

--- fashup/src/components/Header/Header.jsx
+++ fashup/src/components/Header/Header.jsx
@@ -1,31 +1,33 @@
 import React from "react";
-import { Button, Box, AppBar, Toolbar, Tabs, Tab } from "@mui/material";
 import "./Header.css";
+import logo from "./logo_tuerkis.png"
+import { Button, Box, AppBar, Toolbar, Tabs, Tab } from "@mui/material";
 import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
 
 const Header = () => {
   return (
     <React.Fragment>
-      <AppBar className="header">
-        <Toolbar>
-          <Box className="logo">FASH UP</Box>
-          <Box className="categories">
-            <Tabs>
+      <AppBar >
+        <Toolbar sx={{ backgroundColor: "#BFEEED"}}>
+          <img src={logo} alt="fashup logo" style={{width: "5%", height: "auto"}}/>
+          
+            <Tabs sx={{marginLeft:"auto"}} >
               <Tab label="About Us"/>
               <Tab label="Our API"/>
               <Tab label="Documentation" />
               <Tab label="Help" />
             </Tabs>
-          </Box>
-          <Box className="signUp">
+          
+          
             <Button
               variant="outlined"
               color="primary"
               startIcon={<PersonAddAltIcon />}
+              sx={{marginLeft:"auto"}}
             >
               Sign Up
             </Button>
-          </Box>
+          
         </Toolbar>
       </AppBar>
     </React.Fragment>
