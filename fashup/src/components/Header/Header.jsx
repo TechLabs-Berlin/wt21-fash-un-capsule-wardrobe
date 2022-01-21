599104f1cd024121ee4d152ca2da7967c1460905
Cem <ulrich_345@web.de>
2022-01-21 23:42:04 +0100

    import useState and add onChange function  to Tabs

--- fashup/src/components/Header/Header.jsx
+++ fashup/src/components/Header/Header.jsx
@@ -1,33 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
-import logo from "./logo_tuerkis.png"
-import { Button, Box, AppBar, Toolbar, Tabs, Tab } from "@mui/material";
+import logo from "./logo_tuerkis.png";
+import { Button, AppBar, Toolbar, Tabs, Tab } from "@mui/material";
 import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
 
+
 const Header = () => {
+  const [value, setValue] = useState(0);
+
   return (
     <React.Fragment>
-      <AppBar  elevation={0}>
-        <Toolbar sx={{ backgroundColor: "#BFEEED"}}>
-          <img src={logo} alt="fashup logo" style={{width: "5%", height: "auto"}}/>
-          
-            <Tabs sx={{marginLeft:"auto"}} >
-              <Tab variant="h3" label="About Us"/>
-              <Tab label="Our API"/>
-              <Tab label="Documentation" />
-              <Tab label="Help" />
-            </Tabs>
-          
-          
-            <Button
-              variant="outlined"
-              color="primary"
-              startIcon={<PersonAddAltIcon />}
-              sx={{marginLeft:"auto"}}
-            >
-              Sign Up
-            </Button>
-          
+      <AppBar elevation={0}>
+        <Toolbar sx={{ backgroundColor: "#BFEEED" }}>
+          <img src={logo} alt="fashup logo" style={{ width: "5%", height: "auto" }} />
+
+          <Tabs sx={{ marginLeft: "auto" }} value={value} onChange={(e, value) => setValue(value)} indicatorColor="primary">
+            <Tab label="About Us" />
+            <Tab label="Our API" />
+            <Tab label="Documentation" />
+            <Tab label="Help" />
+          </Tabs>
+
+          <Button variant="outlined" color="primary" startIcon={<PersonAddAltIcon />} sx={{ marginLeft: "auto" }}>
+            Sign Up
+          </Button>
         </Toolbar>
       </AppBar>
     </React.Fragment>
