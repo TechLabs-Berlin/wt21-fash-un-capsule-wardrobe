54f4ec2d79b24b3ff1ca4ebc9c7f04ae81d7f3eb
Lukas <lukas-bauerschmidt@web.de>
2022-01-22 12:11:07 +0100

    Fix directory errors

--- AI/app.py
+++ AI/app.py
@@ -21,7 +21,7 @@ import os
 
 app = Flask(__name__)
 model = torchvision.models.resnet50(pretrained=True) # load ResNet50
-database_path = "./database/*.jpeg"
+database_path = "/Users/lukasbauerschmidt/Desktop/TechLab/3. Project/3. Code/wt21-fash-un-capsule-wardrobe/AI/database/*.jpeg"
 
 def preproc(d: DocumentArray):
         return (d.load_uri_to_image_tensor()  # load
@@ -42,7 +42,7 @@ def about():
 @app.route('/', methods=['POST'])
 def predict():
     imagefile = request.files['imagefile']
-    image_path = "static/images/" + imagefile.filename
+    image_path = "/Users/lukasbauerschmidt/Desktop/TechLab/3. Project/3. Code/wt21-fash-un-capsule-wardrobe/AI/static/images/" + imagefile.filename
     imagefile.save(image_path)
     left_da = DocumentArray.from_files(image_path)
     left_da.apply(preproc)
@@ -58,7 +58,7 @@ def predict():
         for m in d.matches:
             print(d.uri, m.uri, m.scores['cosine'].value)
 
-    classification = (DocumentArray(left_da[0].matches, copy=True).apply(lambda d: d.set_image_tensor_channel_axis(0, -1).set_image_tensor_inv_normalization()).plot_image_sprites(output='static/images/result.jpeg'))
+    classification = (DocumentArray(left_da[0].matches, copy=True).apply(lambda d: d.set_image_tensor_channel_axis(0, -1).set_image_tensor_inv_normalization()).plot_image_sprites(output='/Users/lukasbauerschmidt/Desktop/TechLab/3. Project/3. Code/wt21-fash-un-capsule-wardrobe/AI/static/images/result.jpeg'))
 
     return render_template('index.html', prediction = 1)
     
