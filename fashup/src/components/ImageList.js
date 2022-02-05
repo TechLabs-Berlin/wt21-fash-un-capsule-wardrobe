451efb645ef6c6196445d106415a24bf04b609a9
Lukas <lukas-bauerschmidt@web.de>
2022-02-05 14:44:25 +0100

    Import test pictures

--- fashup/src/components/ImageList.js
+++ fashup/src/components/ImageList.js
@@ -118,7 +118,7 @@ export default function SearchResults({ imagePaths }) {
                       color="green"
                       fontWeight="bold"
                     >
-                      {item.price}
+                      ${item.price}
                     </Typography>
                   </Grid>
                 </Grid>
